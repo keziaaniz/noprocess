5e1eaf411d4fcd99e1a5ebba172accf2164cd495
Kézia Máren <89536426+keziaaniz@users.noreply.github.com>
2022-05-27 22:21:25 -0300

    Delete Modal.js

--- src/components/Modal.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-<div id="Form-button" className="Modal">{modalVisible &&
-    <div id="Form-button" className="Modal">
-        <button class="close" type="button" >X</button>
-        <p>Obrigado pelo contato ${`name`}</p>
-    </div>
-}
-</div> 
